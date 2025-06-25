1334c286db6dec564e51ff3fd49826b6e5d34703
Joonatan Uusväli <1826966+Seramis@users.noreply.github.com>
2025-06-25 18:52:40 +0300

    Make Ulid AoT compatible (#12)
    Mark Ulid library as AoT compatible.
    Add AoT Console application to use Ulid in various ways to test AoT compatibility.
    Added GitHub Action workflow to run the AoT Console application.

---
+++ src/ByteAether.Ulid.Tests.AotConsole/UlidJsonContext.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ByteAether.Ulid;
+
+[JsonSerializable(typeof(Ulid))]
+[JsonSerializable(typeof(MyClassWithUlid))]
+internal partial class UlidJsonContext : JsonSerializerContext
+{ }
